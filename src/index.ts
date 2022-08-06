c1adb10fa1f8f004bb9fd04c0ed4a7824757b264
JuanMGitHub <104040052+JuanMGitHub@users.noreply.github.com>
2022-08-06 16:05:45 -0300

    ejercicio_para_entregar lo que hicimos ultimo

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,26 @@ document.getElementById("app").innerHTML = `
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>`;*/
-console.log("mostrarJuan");
 //PROGRAMA SUMA ENTRE NUMEROS
+let numMenor, numMayor, numero1, numero2: number;
+numero1 = Number(prompt("ingrese el primer número del rango a sumar: "));
+numero2 = Number(prompt("ingrese el segundo número del rango a sumar: "));
+if (numero1 <= numero2) {
+  numMenor = numero1;
+  numMayor = numero2;
+} else {
+  numMenor = numero2;
+  numMayor = numero1;
+}
+console.log("el numero menor elegido fue: " + numMenor);
+console.log("el numero mayor elegido fue: " + numMayor);
+for (
+  let indice: number = 1, numSuma: number = numMenor;
+  numMenor < numMayor;
+  indice++
+) {
+  numMenor = numMenor + 1;
+  console.log("el numero del rango a sumar: " + numMenor);
+  numSuma = numSuma + numMenor;
+  console.log("la suma parcial del rango es: " + numSuma);
+}
